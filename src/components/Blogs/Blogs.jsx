69ca647245e635381210ca9606d482f09444cb15
Tasin1025 <tasintt@gmail.com>
2023-09-13 18:00:54 +0600

    blog added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -10,8 +10,12 @@ const Blogs = () => {
     },[])
 
     return (
-        <div>
+        <div className="md: w-2/3">
            <h1 className="text-4xl"> Blogs: {blogs.length} </h1> 
+
+            {
+                blogs.map()
+            }
            
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import header from "../../assets/images/profile.png"
 
 const Header = () => {
     return (
-        <div>
-             <h1 className='text-4xl text-bold'> Knowledge cafe </h1>
+        <div className="flex justify-between p-4 mx-4 my-1 border-b-2">
+             <h1 className='text-4xl font-bold'> Knowledge cafe </h1>
+             <img src= {header} alt="picture" />
         </div>
     );
 };
